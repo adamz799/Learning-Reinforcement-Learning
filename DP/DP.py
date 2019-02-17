755a933d668bbfb2fa609690dc78654387917d41
adamz799 <zhqbdfx@126.com>
2019-02-17 18:59:07 +0800

    Update and rename policy_evaluation.py to DP.py

--- DP/DP.py
+++ DP/DP.py
@@ -49,7 +49,38 @@ def policy_evaluation(policy, env, gamma = 0.95, theta = 1e-5):
         #Check whether delta<theta
         if flag or iteration>10000:
             break
-
     return np.array(V)
+  
+  
+def policy_improvement(policy, env, V, gamma=0.95):
+    def q(s,a):
+        r1=0
+        for t in env.P[s][a]:
+                # P[s][a] = (prob, next_state, reward, is_done)
+                r1 += t[0]*(t[2] + gamma*V[1])
+        return r1
 
-random_policy = np.ones([env.nS, env.nA])/env.nA
+    policy_stable=1
+    for s in env.nS:
+        old_action = np.argmax(policy[s])    
+        q_val = np.array([q(s,a) for a in range(env.nA)])
+        new_action = np.argmax(q_val)
+        for a in range(env.nA):
+            policy[s][a]= 1 if a==new_action else 0
+        if old_action!= new_action:
+            policy_stable=0
+
+    if policy_stable:
+        return policy
+
+
+def policy_itreation(env, policy=None ,gamma = 0.95, theta = 0.001):
+    #Initialization
+    if policy == None:
+        policy = np.ones([env.nS, env.nA])/env.nA#Random policy
+    
+    while 1:
+        #Policy Evaluation
+        V = policy_evaluation(policy, env, gamma = gamma, theta=theta)
+        #Policy Improvement
+        policy=policy_improvement(policy, env, V, gamma = gamma)
